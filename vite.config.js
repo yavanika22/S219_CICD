f38ba18f78e72b7016c3637fea0bed2730debcd4
githubelan77 <elan77@gmail.com>
2025-07-22 10:37:43 +0530

    V1.0.1

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/S219_CICD/'
+});
